faecf6859ea79fcc7ff249c193d0652dfd4fbc39
douhuai <122762659+douhuai@users.noreply.github.com>
2026-05-14 09:28:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const OPT = {
     "user" : "admin", //后台密码请修改
     "password" : "yourSelfPwd", //后台密码
-    "siteDomain" : "blog.gezhong.vip",// 域名(不带https 也不带/)
+    "siteDomain" : "blog.731777.xyz",// 域名(不带https 也不带/)
     "siteName" : "CF workers blog",//博客名称
     "siteDescription":"A Blog Powered By Cloudflare Workers and KV",//博客描述
     "keyWords":"cloudflare,KV,workers,blog",//关键字
@@ -15,9 +15,31 @@ const OPT = {
     "cacheTime" : 60*60*24*0.5, //网页缓存时长(秒),建议=文章更新频率
     "themeURL" : "https://raw.githubusercontent.com/gdtool/cloudflare-workers-blog/master/themes/default2.0/", // 模板地址,以 "/"" 结尾
     "html404" : `<b>404</b>`,//404页面代码
-    "codeBeforHead":``,//其他代码,显示在</head>前
+    "codeBeforHead": `<link rel="stylesheet" href="https://unpkg.com/@waline/client@v3/dist/waline.css" />
+		<script type="module">
+		  // 注意：官方文档中使用的文件是 waline.js，而不是 waline.mjs
+		  import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
+
+		  window.initWaline = () => {
+			init({
+			  el: '#waline-comment',
+			  serverURL: 'https://api.731777.xyz', // 请确认这是你正确的后端地址
+			  path: window.location.pathname,
+			});
+		  };
+		</script>`,//其他代码,显示在</head>前
     "codeBeforBody":``,//其他代码,显示在</body>前
-    "commentCode":``,//评论区代码
+    "commentCode":`<div id="waline-comment" class="comment"></div>
+		<script type="module">
+		  if (window.initWaline) {
+			window.initWaline();
+		  } else {
+			// 如果脚本尚未加载完成，等待 DOM 加载后再尝试
+			document.addEventListener('DOMContentLoaded', () => {
+			  if (window.initWaline) window.initWaline();
+			});
+		  }
+		</script>`,//评论区代码
     "widgetOther":``,//20201224新增参数,用于右侧 小部件扩展
     "otherCodeA":``,//其他参数A,可设置为 "阅读次数:"四个大字
     "otherCodeB":``,//其他参数A
